16d020c57de45671e2c68afcd50af5586b89b322
jackson-lenhart <jacksonlenhart@gmail.com>
2017-12-10 19:11:32 -0500

    fix delete user

--- client.js
+++ client.js
@@ -112,63 +112,35 @@ module.exports = ((http, fs, request) => {
         if (err) throw err;
         console.log("Body:", body);
       });
-
-      /*http.get("http://localhost:4567/secret", (res) => {
-        const statusCode = res.statusCode;
-        const contentType = res.headers["content-type"];
-
-        let error;
-        if (statusCode !== 200) {
-          error = new Error("Request Failed.\n" +
-                            `Status Code: ${statusCode}`);
-        } else if (!/^text\/html/.test(contentType)) {
-          error = new Error("Invalid content type.\n" +
-                            `Expected application/json but received ${contentType}`);
-        }
-        if (error) {
-          console.error(error.message);
-          res.resume();
-          return;
-        }
-
-        res.setEncoding("utf8");
-        let rawData = "";
-        res.on("data", (chunk) => { rawData += chunk; });
-        res.on("end", () => {
-          try {
-            console.log(rawData);
-          } catch (e) {
-            console.error(e.message);
-          }
-        });
-      }).on("error", (e) => {
-        console.error(`Got error: ${e.message}`);
-      });*/
     },
     deleteUser: (user, password) => {
-      const options = {
-        hostname: "localhost",
-        port: 4567,
-        path: `/delete/${user}/${password}`,
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Content-Length": Buffer.byteLength(user + password)
-        }
+      if (!STATE.signedIn) {
+        console.error("You must be signed in to delete account");
+        return;
+      }
+
+      const userToDelete = {
+        user,
+        password
       };
 
-      const req = http.request(options, (res) => {
-        res.setEncoding("utf8");
-        res.on("data", (chunk) => {
-          console.log(chunk);
-        });
-      });
+      const options = {
+        url: "http://localhost:4567/delete",
+        method: "POST",
+        json: userToDelete
+      };
 
-      req.on("error", (e) => {
-        console.error(`problem with request: ${e.message}`);
-      });
+      request(options, (err, res, body) => {
+        if (err) throw err;
+        console.log("Body:", body);
 
-      req.end();
+        if (!body.success) {
+          throw new Error(body.msg);
+        }
+
+        STATE.signedIn = false;
+        STATE.currentUser = null;
+      });
     },
     undeleteUser: (user, password) => {
       const options = {
--- index.js
+++ index.js
@@ -73,7 +73,6 @@ app.post("/createuser", jsonParser, (req, res) => {
   });
 });
 
-//promisify sign in
 app.post("/signin", jsonParser, (req, res) => {
   const { user, password } = req.body;
 
@@ -84,12 +83,20 @@ app.post("/signin", jsonParser, (req, res) => {
       if (!result) {
         res.send("User does not exist");
         db.close();
+        return;
+      }
+
+      if (result.deleted) {
+        res.json({ msg: `${user} has been deleted`, success: false });
+        db.close();
+        return;
       }
 
       bcrypt.compare(password, result.password, (err, success) => {
         if (err) {
           res.send("Password does not match our records");
           db.close();
+          return;
         };
 
         if (success) {
@@ -104,6 +111,8 @@ app.post("/signin", jsonParser, (req, res) => {
           res.json({ sessionId, timestamp, success: true });
         } else {
           res.json({ msg: "Password does not match our records", success: false });
+          db.close();
+          return;
         }
       });
     }).catch((err) => {
@@ -122,17 +131,26 @@ app.post("/changepassword", jsonParser, (req, res) => {
       if (!result) {
         res.send("User does not exist");
         db.close();
+        return;
+      }
+
+      if (result.deleted) {
+        res.json({ msg: `${user} has been deleted`, success: false });
+        db.close();
+        return;
       }
 
       bcrypt.compare(password, result.password, (err, success) => {
         if (err) {
           console.error(err);
           db.close();
+          return;
         }
 
         if (!success) {
           res.send("Password does not match our records");
           db.close();
+          return;
         }
 
         userUtils.hashPassword(newPassword).then((hash) => {
@@ -168,35 +186,61 @@ app.get("/secret", (req, res) => {
   res.send("Secret page!");
 });
 
-app.post("/delete/:user/:password", (req, res) => {
-  const USERS = userUtils.parseUsers();
+app.post("/delete", jsonParser, (req, res) => {
+  const { user, password } = req.body;
 
-  const { user, password } = req.params;
-  let userExists = false;
-  for (let i = 0; i < USERS.length; i++) {
-    if (USERS[i].user === user) {
-      userExists = true;
-      if (USERS[i].deleted === "true") {
-        res.send(`${user} has already been deleted.`);
+  MongoClient.connect(mongoUrl).then((db) => {
+    const Users = db.collection("users");
+
+    Users.findOne({ user: user }).then((result) => {
+      if (!result) {
+        res.json({ msg: "Could not find user", success: false });
+        db.close();
         return;
       }
 
-      userUtils.hashPassword(password, (passwordDigest) => {
-        if (USERS[i].passwordDigest === passwordDigest) {
-          userUtils.deleteUser(USERS[i].id);
-          res.send(`Successfully deleted ${user}`);
-          STATE.signedIn = false;
-        } else {
-          res.send("Password does not match our records");
-        }
-      });
-      break;
-    }
-  }
+      if (result.deleted) {
+        res.json({ msg: "User has already been deleted", success: false });
+        db.close();
+        return;
+      }
 
-  if (!userExists) {
-    res.send("User does not exist");
-  }
+      bcrypt.compare(password, result.password, (err, success) => {
+        if (err) {
+          console.error(err);
+          db.close();
+          return;
+        }
+
+        if (!success) {
+          res.json({ msg: "Password does not match our records", success: false });
+          db.close();
+          return;
+        }
+
+        Users.update(
+          { user: result.user },
+          {
+            user: result.user,
+            password: result.password,
+            deleted: true
+          }
+        ).then((result) => {
+          res.json({ msg: `User ${user} successfully deleted`, success: true });
+          db.close();
+        }).catch((err) => {
+          console.error(err);
+          res.json({ msg: `${user} could not be deleted`, success: false });
+          db.close();
+        });
+      });
+    }).catch((err) => {
+      console.error(err);
+      db.close();
+    });
+  }).catch((err) => {
+    console.error(err);
+  });
 });
 
 app.post("/undelete/:user/:password", (req, res) => {
